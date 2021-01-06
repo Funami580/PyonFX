d9a894824d0ed8d306f02191fdac02b70701cbd0
Funami580 <Funami580@users.noreply.github.com>
2021-01-06 11:36:51 +0100

    Remove Python 3.6 + 3.7 support Remove macOS workflow

--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ setuptools.setup(
         "Development Status :: 2 - Pre-Alpha",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.8",
     ],
 )
