b5c2d0fba229f6f95eefb1f3291cd5a2b1112b02
Funami580 <Funami580@users.noreply.github.com>
2021-01-06 13:50:50 +0100

    Try to fix lint and imports for GitHub workflow

--- pyonfx/font_utility.py
+++ pyonfx/font_utility.py
@@ -21,11 +21,11 @@ to help getting informations from a specific font
 import sys
 
 if "sphinx" not in sys.modules:
-    import pyonfx.python_ass.ass as ass
-    import pyonfx.python_ass.ass.renderer
+    import python_ass.ass as ass
+    import python_ass.ass.renderer
     from datetime import timedelta
 
-    _context = pyonfx.python_ass.ass.renderer.Context()
+    _context = python_ass.ass.renderer.Context()
     _renderer = _context.make_renderer()
     _renderer.set_fonts(fontconfig_config="\0")
     _track = _context.make_track()
@@ -68,14 +68,14 @@ class Font:
                     fr"{scale_x}{scale_y}{spacing}{shadow}{outline}}}{input_text}")
 
         meta = line.styleref.assref.meta
-        doc = pyonfx.python_ass.ass.document.Document()
+        doc = python_ass.ass.document.Document()
 
-        doc.styles.append(pyonfx.python_ass.ass.document.Style(
+        doc.styles.append(python_ass.ass.document.Style(
             name="Default",
             primary_color=ass.data.Color.BLACK
         ))
 
-        doc.events.append(pyonfx.python_ass.ass.document.Dialogue(
+        doc.events.append(python_ass.ass.document.Dialogue(
             start=timedelta(0),
             end=timedelta(milliseconds=1),
             style="Default",
